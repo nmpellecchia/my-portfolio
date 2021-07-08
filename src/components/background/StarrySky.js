b2b3f6f7f14b97cbe4bb5f4dc2319811f978c084
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-08 17:54:46 -0300

    added sky gradient and stars

--- src/components/background/StarrySky.js
+++ src/components/background/StarrySky.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import './StarrySky.css';
 
 function StarrySky() {
   const addStars = amount => {
     const stars = [];
     for (let i = 0; i <= amount; i++) {
-      stars.push(<Star />);
+      stars.push(<Star key={i.toString()} />);
     }
     return stars;
   };
-  return <div>{addStars(20)}</div>;
+  return <div className="night-sky">{addStars(30)}</div>;
 }
 
 function Star() {
   return (
     <div
       className="star"
-      /* style={{ animation: 'twinkle 5s linear infinite' }} */
-    >
-      *
-    </div>
+      style={{
+        /* prettier-ignore */ animation: `twinkle ${( Math.random() * 5 + 3).toString()}s linear infinite`,
+        top: `${Math.random() * window.innerHeight}px`,
+        left: `${Math.random() * window.innerWidth}px`,
+      }}
+    ></div>
   );
 }
 
