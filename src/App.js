63dd8bdde9ae91c2dde207278ca78a0fe3da766c
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-11 18:33:30 -0300

    added glass panels

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import StarrySky from './components/background/StarrySky.js';
+import GlassPanel from './components/windows/GlassPanel';
 
 function App() {
   return (
     <div className="App">
-      <p>A</p>
+      <GlassPanel></GlassPanel>
+      {/* <p>A</p> */}
       {/* BG needs to be the last element and be fixed */}
       <StarrySky />
     </div>
---
+++ src/components/windows/GlassPanel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './GlassPanel.css';
+
+/* props with className "type" if it's text css will have different styling than mosaics grid */
+/* projects and mosaics for techs, make them b&w. colors when hovered */
+function GlassPanel() {
+  return (
+    <div className="container">
+      <section className="panel">
+        <div>Hola</div>
+      </section>
+    </div>
+  );
+}
+
+export default GlassPanel;
