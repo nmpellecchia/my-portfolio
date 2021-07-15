af6627e20c570a0daed241523a62843022d4ee25
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-15 17:59:39 -0300

    removed range input
    I don't like the way it works. So I'm changing it to something more intuitive to the users

--- src/components/infoDisplay/Projects.js
+++ src/components/infoDisplay/Projects.js
@@ -4,18 +4,33 @@ import './Projects.css';
 /* props with className "type" if it's text css will have different styling than mosaics grid */
 /* projects and mosaics for techs, make them b&w. colors when hovered */
 function GlassPanel(props) {
-  /* const projects = [];
-  for (let i = 0; i < props.projects.length - 1; i++) {
-    projects.push(
-      <ProjectSlide key={`project-${i}`} project={props.project[i]} />
-    );
-  } */
+  const [rangeVal, setRangeVal] = useState('0');
+
+  function handleChange(event) {
+    console.log(rangeVal);
+    setRangeVal(event.target.value);
+    console.log(rangeVal);
+  }
+
   return (
     <div className="container">
       {/* use props.children to pass children */}
       <section>
         {/* props.children */}
-        {/* <ProjectSlide project={props.project}/> */}
+        {/* I don't like input range behaviour, So I'll change it   */}
+        {/* <input
+          className="swapper-slider"
+          type="range"
+          min="-1"
+          max={props.projects.length}
+          step="1"
+          role="slider"
+          defaultValue="0"
+          title="See Projects"
+          onChange={handleChange}
+        /> */}
+
+        {/* <ul className="projects swapper-items">
         {props.projects.map((project, i) => {
           return (
             <ProjectSlide
@@ -25,17 +40,19 @@ function GlassPanel(props) {
             />
           );
         })}
+        </ul> */}
       </section>
     </div>
   );
 }
 
 function ProjectSlide({
+  /* destructuring to avoid using props. ... */
   project: { title, img, tools, desc, repo_url, live_url },
   index,
 }) {
   return (
-    <div className="project-slide" data-key={index}>
+    <li className="project-slide" data-key={index}>
       <div className="project-media">
         <p className="project-title">{title}</p>
         <img src={img} alt={title} className="project-img" />
@@ -50,8 +67,22 @@ function ProjectSlide({
         {/* Only render this btn if the project can be seen live */}
         {/* props.hasLive && <button>View live</button> */}
       </div>
-    </div>
+    </li>
   );
 }
+
+/* function SlideSwapper() {
+  return (
+    <input
+      className="swapper-slider"
+      type="range"
+      min="0"
+      max="4"
+      role="slider"
+      defaultValue="0"
+      title="See Projects"
+    />
+  );
+} */
 
 export default GlassPanel;
