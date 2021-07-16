54771bab467f58ebc991576a84ba5feb2afc62be
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-16 17:52:25 -0300

    added titles css
    I think in this case it's not needed to have the titles as their own component.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { projects } from './components/infoDisplay/projectInstances';
 function App() {
   return (
     <div className="App">
+      <h1 className="big-title">Projects</h1>
       <Projects projects={projects} />
       {/* BG needs to be the last element and be fixed */}
       <StarrySky />
