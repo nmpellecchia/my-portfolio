29bc04536baf80a4c9964b906c8547edf1f4cb3c
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-16 17:39:39 -0300

    Minor fixes
    - Changed name of obsolete component
    - Removed commented code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import StarrySky from './components/background/StarrySky.js';
-import GlassPanel from './components/infoDisplay/Projects';
+import Projects from './components/infoDisplay/Projects';
 import { projects } from './components/infoDisplay/projectInstances';
 
 function App() {
   return (
     <div className="App">
-      <GlassPanel projects={projects} />
+      <Projects projects={projects} />
       {/* BG needs to be the last element and be fixed */}
       <StarrySky />
     </div>
--- src/components/infoDisplay/Projects.js
+++ src/components/infoDisplay/Projects.js
@@ -1,28 +1,13 @@
 import { React, useState } from 'react';
 import './Projects.css';
 
-/* props with className "type" if it's text css will have different styling than mosaics grid */
-/* projects and mosaics for techs, make them b&w. colors when hovered */
-function GlassPanel(props) {
+function Projects(props) {
   const [rangeVal, setRangeVal] = useState(0);
 
   return (
     <div className="container">
       {/* use props.children to pass children */}
       <section>
-        {/* props.children */}
-        {/* I don't like input range behaviour, So I'll change it   */}
-        {/* <input
-          className="swapper-slider"
-          type="range"
-          min="-1"
-          max={props.projects.length}
-          step="1"
-          role="slider"
-          defaultValue="0"
-          title="See Projects"
-          onChange={handleChange}
-        /> */}
         <div className="swapper-sliders">
           <button
             className="swapper-slider swapper-previous-slide"
@@ -45,18 +30,6 @@ function GlassPanel(props) {
           <p>{rangeVal}</p>
         </div>
         <ProjectSlide project={props.projects[rangeVal]} index="2" />
-
-        {/* <ul className="projects swapper-items">
-        {props.projects.map((project, i) => {
-          return (
-            <ProjectSlide
-              key={`project-${i.toString()}`}
-              project={project}
-              index={i}
-            />
-          );
-        })}
-        </ul> */}
       </section>
     </div>
   );
@@ -79,12 +52,11 @@ function ProjectSlide({
       </div>
       <div className="project-buttons">
         <a href={repo_url}>View repository</a>
-        <a href={live_url}>View repository</a>
         {/* Only render this btn if the project can be seen live */}
-        {/* props.hasLive && <button>View live</button> */}
+        live_url && <a href={live_url}>View repository</a>
       </div>
     </li>
   );
 }
 
-export default GlassPanel;
+export default Projects;
