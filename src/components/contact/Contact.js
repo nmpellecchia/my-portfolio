3d743c51a64d00c620343cb4ad4067d603b4c6bd
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-19 17:55:28 -0300

    fixes
    - Added correct tags to socials
    - Changed box-shadow of socials
    - Moved the floating keyframes for better accesibility

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -8,13 +8,26 @@ function Contact() {
       <footer>
         <div className="contacts-outer">
           <h2 className="contact-txt">
-            Want to talk or work with me? -Send me an email- or a message on
-            these online spaces!
+            Want to talk or work with me?
+            <a href="mailto:nicolasmpellecchia@gmail.com">Send me an email</a>
+            or a message on these online spaces!
           </h2>
           <ul className="contact-socials">
-            <li>github</li>
-            <li>codepen</li>
-            <li>linkedin</li>
+            <li>
+              <a href="" rel="external">
+                <i className="devicon-codepen-plain"></i>
+              </a>
+            </li>
+            <li>
+              <a href="" rel="external">
+                <i className="devicon-github-original"></i>
+              </a>
+            </li>
+            <li>
+              <a href="" rel="external">
+                <i className="devicon-linkedin-plain"></i>
+              </a>
+            </li>
           </ul>
         </div>
       </footer>
