56a046258f0172e21439e65bee03800f60f11b42
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-07-25 18:04:29 -0300

    name

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { projects } from './components/infoDisplay/projectInstances';
 import Toolbox from './components/infoDisplay/Toolbox';
 import Contact from './components/contact/Contact';
 import AboutMe from './components/infoDisplay/About';
+import MyName from './components/name/Name';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="big-title">Projects</h1>
+      <MyName />
+      {/* <h1 className="big-title">Projects</h1>
       <Projects projects={projects} />
       <h1 className="big-title">ToolBox</h1>
       <Toolbox />
       <h1 className="big-title">About me</h1>
       <AboutMe />
-      <Contact />
+      <Contact /> */}
       {/* BG needs to be the last element and pos. fixed */}
       <StarrySky />
     </div>
